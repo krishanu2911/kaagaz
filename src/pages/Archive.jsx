f771a9d42724cb86300a9f6cfd7900df295e4b3c
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-19 12:29:42 +0530

    Feat: Added Pages

---
+++ src/pages/Archive.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Archive() {
+    return (
+        <div>
+           <h1>Archive page</h1> 
+        </div>
+    )
+}
+export default Archive;
---
+++ src/pages/Trash.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Trash() {
+    return (
+        <div>
+            <h1>Trash page</h1>
+        </div>
+    )
+}
+export default Trash;
