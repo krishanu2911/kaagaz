ff63731e91b2ff32733fc04f540f8ff789f1fafb
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-24 01:59:31 +0530

    Fix: Topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import styles from "./Topbar.module.css"
-// import { Fa } from "react-icons/fa";
+import React from "react";
+import styles from "./Topbar.module.css";
+import { IoApps } from "react-icons/io5";
+import { Link } from "react-router-dom";
 function Topbar() {
-    return (
-        <div className={`${styles.topbar} maple-flex`}>
-            {/* <FaHamburger /> */}
-           <h1 className={`${styles.title} txt-xxl`}>Kaagaz</h1>
-           <section>
-             
-           </section> 
-        </div>
-    )
+  return (
+    <div className={`${styles.topbar} maple-flex`}>
+      <div className={`${styles.aligncenter} maple-flex gap`}>
+        <IoApps className={`${styles.toggleicon} txt-xl`} />
+        <h1 className={`${styles.title} txt-xxl`}>Kaagaz</h1>
+      </div>
+      <div className={`${styles.aligncenter} maple-flex gap`}>
+        <Link className="txt-lg" to="/login">Login</Link>
+        <Link className="txt-lg" to="/signup">Signup</Link>
+      </div>
+    </div>
+  );
 }
 export default Topbar;
